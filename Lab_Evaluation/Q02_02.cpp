12a4a6ffb1530a148c643dd9d1ed416f716d3f41
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-08-03 06:17:54 +0530

    Update Q02_02.cpp
    comment explaining return  added

--- Lab_Evaluation/Q02_02.cpp
+++ Lab_Evaluation/Q02_02.cpp
@@ -1,41 +1,51 @@
 #include <iostream>
 using std::cout, std::cin;
 
-int getn(int arr[], int N, int l, int r){
+int getn(int arr[], int N, int l, int r)
+{
     // cout<<"n:"<<r;
-    if(r>=N){r=N-1;}
-    if (l>=r)
+    if (r >= N)
+    {
+        r = N - 1;
+    }
+    if (l >= r)
     {
         return -1;
     }
-    if (arr[r]<0 && arr[r-1]>=0)
+    if (arr[r] < 0 && arr[r - 1] >= 0)
     {
         return r;
     }
-    else if (arr[l]>=0 && arr[l+1]<0)
+    else if (arr[l] >= 0 && arr[l + 1] < 0)
     {
         return l;
     }
-    else if (arr[l]>=0 && arr[r]>=0){
-        return getn(arr,N,r,r*2);
+    else if (arr[l] >= 0 && arr[r] >= 0)
+    {
+        return getn(arr, N, r, r * 2);
     }
-    else if(arr[l]<0 && arr[r]<0){
-        return getn(arr,N,l/2,r/2);
+    else if (arr[l] < 0 && arr[r] < 0)
+    {
+        return getn(arr, N, l / 2, r / 2);
     }
-    else if (arr[l]>=0 && arr[r]<0){
-        return getn(arr,N,l+1,(l+r)/2);
+    else if (arr[l] >= 0 && arr[r] < 0)
+    {
+        return getn(arr, N, l + 1, (l + r) / 2);
     }
-}
-int main(){
+}//if the return value is -1 there are no position satisfying the condition
+
+
+int main()
+{
     int N;
-    cout<<"Enter size of array:";
-    cin>>N;
+    cout << "Enter size of array:";
+    cin >> N;
     int arr[N];
-    cout<<"Enter elements of array:";
+    cout << "Enter elements of array:";
     for (int i = 0; i < N; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    cout<<"position:"<<getn(arr, N, 0,1);
+    cout << "position:" << getn(arr, N, 0, 1);
     return 0;
 }
