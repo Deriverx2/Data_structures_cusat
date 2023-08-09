9416a25bc9f72d75f0a1c17915e089929b82f96c
E B Benson <117281795+Deriverx2@users.noreply.github.com>
2023-08-09 15:15:21 +0530

    Create Q_02.cpp

---
+++ Assignment_2/Q_02.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using std::cout,std::cin,std::endl;
+
+void insSort(unsigned int* arr, int N){
+    for (int i = 1; i < N; i++) {
+        double key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+    
+}
+
+
+int main()
+{
+    int N;
+    cout<<"Enter number of Elements:";
+    cin>>N;
+    unsigned int arr[N];
+    cout<<"ENter elements";
+    for (int i = 0; i < N; i++) {
+        cin>>arr[i];
+    }
+    cout<<"Before sort:";
+    for (int i = 0; i < N; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    insSort(arr,N);
+    cout<<"After sort:";
+    for (int i = 0; i < N; i++) {
+        cout<<arr[i]<<" ";
+    }
+    
+}
